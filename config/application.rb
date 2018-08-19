0320ec6efac45256388700b239af4ce3147665c9
codingkasia <codingkasia@gmail.com>
2018-08-19 14:05:00 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module MemeGenerator
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -30,6 +36,8 @@ module MemeGenerator
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+
+    # ????
+    # config.api_only = true
   end
 end
